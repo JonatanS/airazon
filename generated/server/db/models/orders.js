ce94fe09aa0236d9f348ededf3bf41d85e43ac50
everett980 <everett980@gmail.com>
2016-01-11 16:40:16 -0500

    finished schemas aside from pre save and post save hooks

--- generated/server/db/models/orders.js
+++ generated/server/db/models/orders.js
@@ -4,12 +4,14 @@ var mongoose = require('mongoose');
 var _ = require('lodash');
 
 var schema = new mongoose.Schema({
-		products: [{pricePaid: {type: Number, required}, reference: {type: mongoose.Schema.Types.ObjectId, ref: 'Product'}}],
-	user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+		products: [{pricePaid: {type: Number, required}, referenceId: {type: mongoose.Schema.Types.ObjectId, ref: 'Product'}}],
+	userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+	trackingNumber: Number,
+	orderStatus: {type: String, enum: ['In Transit', 'Delivered', 'Processing'], default: 'Processing'}
 });
 
-schema.method.getTotal = function() {
+schema.virtual('orderTotal').get(function() {
 		return this.products.reduce( (prev.pricePaid, curr.pricePaid) => prev.pricePaid + curr.pricePaid );
-}	
+});
 
 mongoose.model('Order', schema);
