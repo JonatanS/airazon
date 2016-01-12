09a688bb3adf0c39a7eaaa513f21d05385bde3cd
wnmprk <wonmipark@gmail.com>
2016-01-12 12:29:09 -0500

    changes to product route

--- server/app/routes/users.js
+++ server/app/routes/users.js
@@ -1,4 +1,15 @@
 'use strict';
 var router = require('express').Router();
 module.exports = router;
-var _ = require('lodash');
+var mongoose = require('mongoose');
+var models = require('../../db/models');
+var Promise = require('bluebird');
+var User = models.User;
+
+router.get('/', function (req, res, next) {
+	return User.find({})
+	.then(function (users) {
+		res.send(users);
+	})
+	.then(null, next);
+});
