76b9849f29e2b1da4df97d0356f4f0d308415dce
Joe Alves <joe@fullstackacademy.com>
2016-01-14 12:00:41 -0500

    Wipe commit

--- browser/js/products/products.controller.js
+++
@@ -1,6 +0,0 @@
-app.controller('ProductCtrl', function($scope, $state, ProductFactory, $http) {
-		console.log('Logging from the product controller');
-		$http.get('/api/products/').then(function(products) {
-				$scope.productArr = products.data;
-		});
-});
--- browser/js/products/products.directive.js
+++
@@ -1,9 +0,0 @@
-app.directive('product', function() {
-		return {
-				restrict: 'E',
-				scope: { thisProduct: '=banana' },
-				templateUrl: 'js/products/products.directive.html',
-				link: function(scope, element, attr) {
-				}
-		}
-});
