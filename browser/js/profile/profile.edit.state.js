b911b886398a290d9f3f472449c7fa0b9e900aaf
wnmprk <wonmipark@gmail.com>
2016-01-16 18:32:30 -0500

    user page html edits

--- browser/js/profile/profile.edit.state.js
+++
@@ -1,7 +0,0 @@
-app.config(function ($stateProvider) {
-    $stateProvider.state('profile-edit', {
-        url: '/edit',
-        templateUrl: 'js/profile/profile.edit.html',
-        controller: 'ProfileCtrl'
-    });
-});
