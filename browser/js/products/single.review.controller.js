c798f62b0606445c8b50293109c13560c5a147d9
Leon Thorne <ldthorne@brandeis.edu>
2016-01-16 16:15:11 -0500

    added front-end product views

---
+++ browser/js/products/single.review.controller.js
@@ -0,0 +1,9 @@
+app.controller('SingleReviewCtrl', function ($scope, ProductFactory, $stateParams) {
+	ProductFactory.getOne($stateParams.productId)
+	.then(function (product) {
+		console.log(product)
+		$scope.oneProduct = product;
+		$scope.ratingArr = new Array(ProductFactory.getRatings($scope.oneProduct.reviews))
+		$scope.remainder = new Array(5-ProductFactory.getRatings($scope.oneProduct.reviews))
+	});
+})
