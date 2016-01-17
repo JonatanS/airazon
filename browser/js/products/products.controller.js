0caf0b201da26d8d07ed23c4147397a45548f334
everett980 <everett980@gmail.com>
2016-01-17 16:57:54 -0500

    product filter and $state.go with params works!!!!

--- browser/js/products/products.controller.js
+++ browser/js/products/products.controller.js
@@ -2,5 +2,10 @@ app.controller('ProductCtrl', function ($scope, $state, ProductFactory) {
 	ProductFactory.getAll()
 	.then(function (products) {
 		$scope.productArr = products;
+		if($state.params.filter) {
+			$scope.filterParam = $state.params.filter;
+		}
 	});
+});
+
 });
