2a3485016b451d7d06002340da98a2d05b27eb14
Leon Thorne <ldthorne@brandeis.edu>
2016-01-18 15:55:34 -0500

    trying to get callback url from stripe to hit /api/checkout

--- server/app/routes/checkout.js
+++ server/app/routes/checkout.js
@@ -5,7 +5,7 @@ module.exports = router;
 var stripe = require("stripe")("sk_test_ywYTergB6avFv5nS0OBleBxq");
 
 router.use("/", function(req, res, next) {
-	res.send("BEING HIT")
+    res.send("BEING HIT")
     var stripeToken = req.body.stripeToken;
 
     var charge = stripe.charges.create({
