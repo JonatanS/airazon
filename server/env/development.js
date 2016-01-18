b4799b710dadfc09c2d24ed211abf253457da6f2
Leon Thorne <ldthorne@brandeis.edu>
2016-01-18 14:14:19 -0500

    deleted facebook and twitter configs from dev env

--- server/env/development.js
+++ server/env/development.js
@@ -1,16 +1,6 @@
 module.exports = {
   "DATABASE_URI": "mongodb://admin:stackstorepassword@ds043605.mongolab.com:43605/stackstore",
   "SESSION_SECRET": "Optimus Prime is my real dad",
-  "TWITTER": {
-    "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
-    "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
-    "callbackUrl": "INSERT_TWITTER_CALLBACK_HERE"
-  },
-  "FACEBOOK": {
-    "clientID": "INSERT_FACEBOOK_CLIENTID_HERE",
-    "clientSecret": "INSERT_FACEBOOK_CLIENT_SECRET_HERE",
-    "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
-  },
   "GOOGLE": {
     "clientID": '890008756729-8i4k64s7cavsl987j9avi6kgg2sotdai.apps.googleusercontent.com',
     "clientSecret": 'FnxpI5c3nOWP9uL224xeLkNw',
