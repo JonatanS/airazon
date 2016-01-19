6ae76900aaf18f41f021cb0d21f24e24172c44f8
jonatans <jonatan@jschumacher.com>
2016-01-19 12:14:19 -0500

    improved signup process to automatically login

--- browser/js/signup/signup.js
+++ browser/js/signup/signup.js
@@ -10,13 +10,18 @@ app.config(function ($stateProvider) {
 
 app.controller('SignupCtrl', function ($scope, UserFactory, $state) {
 
-  $scope.sendSignup = function(signupInfo){
-    UserFactory.signup(signupInfo)
-    .then(function(){
-        $state.go('home');
-    }).catch(function () {
-        $scope.error = 'Invalid credentials.';
-    });
-  };
-
+    $scope.sendSignup = function(signupInfo){
+        UserFactory.signup(signupInfo)
+        .then(function(loginInfo){
+            $scope.error = null;
+            AuthService.login(loginInfo)
+            .then(function () {
+                $state.go('home');
+            }).catch(function () {
+                $scope.error = 'Invalid login credentials.';
+            });
+        }).catch(function () {
+            $scope.error = 'Invalid login credentials.';
+        });
+    };
 });
